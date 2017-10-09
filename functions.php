7870178e01a4dbc155c9b0a14fb3852d7f836d1b
Macbook Pro 13 <freeaanzee@gmail.com>
2017-10-09 18:29:33 +0200

    Child theme definiëren
    Inclusief enkele standaardtweaks

---
+++ functions.php
@@ -0,0 +1,72 @@
+<?php
+
+	if ( ! defined('ABSPATH') ) exit;
+
+	// Laad het child theme NIET MEER NODIG BIJ STOREFRONT
+	// add_action( 'wp_enqueue_scripts', 'load_child_theme' );
+
+	function load_child_theme() {
+		// In de languages map van het child theme zal dit niet werken (checkt enkel nl_NL.mo) maar fallback is de algemene languages map (inclusief textdomain)
+		load_child_theme_textdomain( 'oft', get_stylesheet_directory().'/languages' );
+	}
+
+	// Sta HTML-attribuut 'target' toe in beschrijvingen van taxonomieën
+	add_action( 'init', 'allow_target_tag', 20 );
+
+	function allow_target_tag() { 
+	    global $allowedtags;
+	    $allowedtags['a']['target'] = 1;
+	}
+
+	// Fixes i.v.m. cURL
+	add_action( 'http_api_curl', 'custom_curl_timeout', 10, 3 );
+	
+	function custom_curl_timeout( $handle, $r, $url ) {
+		// Fix error 28 - Operation timed out after 10000 milliseconds with 0 bytes received (bij het connecteren van Jetpack met Wordpress.com)
+		curl_setopt( $handle, CURLOPT_TIMEOUT, 30 );
+		// Fix error 60 - SSL certificate problem: unable to get local issuer certificate (bij het downloaden van een CSV in WP All Import)
+		curl_setopt( $handle, CURLOPT_SSL_VERIFYPEER, false );
+	}
+	
+	// Allerhande tweaks na het initialiseren van WordPress
+	add_action( 'init', 'remove_unnecessary_actions', 20 );
+
+	function remove_unnecessary_actions() {
+		// Verwijder de productzoeker en de mini-cart in de header rechtsboven
+		remove_action( 'storefront_header', 'storefront_product_search', 40 );
+		remove_action( 'storefront_header', 'storefront_header_cart', 60 );
+
+		// Laat de categorie- en auteurinfo vallen bij blogposts
+		// remove_action( 'storefront_loop_post', 'storefront_post_meta', 20 );
+		// remove_action( 'storefront_single_post', 'storefront_post_meta', 20 );
+
+		// Verwijder alle buttons om te kopen
+		remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart' );
+		remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
+		
+		// Verwijder de default credits in de footer
+		remove_action( 'storefront_footer', 'storefront_credit', 20 );
+	}
+
+	// Print variabelen op een overzichtelijke manier naar debug.log
+	if ( ! function_exists( 'write_log' ) ) {
+		function write_log ( $log )  {
+			if ( true === WP_DEBUG ) {
+				if ( is_array( $log ) || is_object( $log ) ) {
+					error_log( print_r( $log, true ) );
+				} else {
+					error_log( $log );
+				}
+			}
+		}
+	}
+
+	// Definieer overzichtelijkere debugfunctie
+	function var_dump_pre( $variable ) {
+		echo '<pre>';
+		var_dump($variable);
+		echo '</pre>';
+		return null;
+	}
+	
+?>
