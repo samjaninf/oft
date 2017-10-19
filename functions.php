e6e6889ec681129b78eb91e37f81b4faa4d2cad0
Macbook Pro 13 <freeaanzee@gmail.com>
2017-10-19 14:02:32 +0200

    Extra taxonomieën
    En bovendien vertaalbaar!

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 	if ( ! defined('ABSPATH') ) exit;
 
-	// Laad het child theme NIET MEER NODIG BIJ STOREFRONT
+	// Laad het child theme NIET MEER NODIG BIJ STOREFRONT MAAR MISSCHIEN NUTTIG VOOR VERTALINGEN
 	// add_action( 'wp_enqueue_scripts', 'load_child_theme' );
 
 	function load_child_theme() {
@@ -30,7 +30,7 @@
 	
 	// Allerhande tweaks na het initialiseren van WordPress
 	add_action( 'init', 'remove_unnecessary_actions', 20 );
-
+	
 	function remove_unnecessary_actions() {
 		// Verwijder de productzoeker en de mini-cart in de header rechtsboven
 		remove_action( 'storefront_header', 'storefront_product_search', 40 );
@@ -47,6 +47,11 @@
 		// Verwijder de default credits in de footer
 		remove_action( 'storefront_footer', 'storefront_credit', 20 );
 	}
+
+
+	################
+	#  TAXONOMIES  #
+	################
 
 	// Creëer een custom hiërarchische taxonomie op producten om partner/landinfo in op te slaan
 	add_action( 'init', 'register_partner_taxonomy', 0 );
@@ -124,6 +129,201 @@
 		register_taxonomy_for_object_type( $taxonomy_name, 'product' );
 	}
 
+	// Creëer drie custom hiërarchische taxonomieën op producten om wijninfo in op te slaan
+	add_action( 'init', 'register_wine_taxonomy', 0 );
+	
+	function register_wine_taxonomy() {
+		$name = 'druif';
+		$taxonomy_name = 'product_grape';
+		
+		$labels = array(
+			'name' => __( 'Druiven', 'oft' ),
+			'singular_name' => __( 'Druif', 'oft' ),
+			'all_items' => __( 'Alle druivensoorten', 'oft' ),
+			'parent_item' => __( 'Druif', 'oft' ),
+			'parent_item_colon' => __( 'Druif:', 'oft' ),
+			'new_item_name' => __( 'Nieuwe druivensoort', 'oft' ),
+			'add_new_item' => __( 'Voeg nieuwe druivensoort toe', 'oft' ),
+		);
+
+		$args = array(
+			'labels' => $labels,
+			'description' => sprintf( __( 'Voeg de wijn toe aan een %s in de wijnkiezer', 'oft' ), $name ),
+			'public' => false,
+			'publicly_queryable' => true,
+			'hierarchical' => true,
+			'show_ui' => true,
+			'show_in_menu' => false,
+			'show_in_nav_menus' => false,
+			'show_in_rest' => true,
+			'show_tagcloud' => true,
+			'show_in_quick_edit' => false,
+			'show_admin_column' => false,
+			// Geef catmans rechten om zelf termen toe te kennen / te bewerken / toe te voegen
+			'capabilities' => array( 'assign_terms' => 'edit_products', 'edit_terms' => 'edit_products', 'manage_terms' => 'edit_products', 'delete_terms' => 'create_sites' ),
+			// In de praktijk niet bereikbaar op deze URL want niet publiek!
+			'rewrite' => array( 'slug' => $name, 'with_front' => false, 'hierarchical' => false ),
+		);
+
+		register_taxonomy( $taxonomy_name, 'product', $args );
+		register_taxonomy_for_object_type( $taxonomy_name, 'product' );
+
+		unset( $labels );
+		$name = 'gerecht';
+		$taxonomy_name = 'product_recipe';
+		
+		$labels = array(
+			'name' => __( 'Gerechten', 'oft' ),
+			'singular_name' => __( 'Gerecht', 'oft' ),
+			'all_items' => __( 'Alle gerechten', 'oft' ),
+			'parent_item' => __( 'Gerecht', 'oft' ),
+			'parent_item_colon' => __( 'Gerecht:', 'oft' ),
+			'new_item_name' => __( 'Nieuw gerecht', 'oft' ),
+			'add_new_item' => __( 'Voeg nieuw gerecht toe', 'oft' ),
+		);
+
+		$args['labels'] = $labels;
+		$args['description'] = sprintf( __( 'Voeg de wijn toe aan een %s in de wijnkiezer', 'oft' ), $name ),
+		$args['rewrite']['slug'] = $name;
+
+		register_taxonomy( $taxonomy_name, 'product', $args );
+		register_taxonomy_for_object_type( $taxonomy_name, 'product' );
+
+		unset( $labels );
+		$name = 'smaak';
+		$taxonomy_name = 'product_taste';
+		
+		$labels = array(
+			'name' => __( 'Smaken', 'oft' ),
+			'singular_name' => __( 'Smaak', 'oft' ),
+			'all_items' => __( 'Alle smaken', 'oft' ),
+			'parent_item' => __( 'Smaak', 'oft' ),
+			'parent_item_colon' => __( 'Smaak:', 'oft' ),
+			'new_item_name' => __( 'Nieuwe smaak', 'oft' ),
+			'add_new_item' => __( 'Voeg nieuwe smaak toe', 'oft' ),
+		);
+
+		$args['labels'] = $labels;
+		$args['description'] = sprintf( __( 'Voeg de wijn toe aan een %s in de wijnkiezer', 'oft' ), $name ),
+		$args['rewrite']['slug'] = $name;
+
+		register_taxonomy( $taxonomy_name, 'product', $args );
+		register_taxonomy_for_object_type( $taxonomy_name, 'product' );
+	}
+
+	// Creëer een custom hiërarchische taxonomie op producten om allergeneninfo in op te slaan
+	add_action( 'init', 'register_allergen_taxonomy', 0 );
+
+	function register_allergen_taxonomy() {
+		$taxonomy_name = 'product_allergen';
+		
+		$labels = array(
+			'name' => __( 'Allergenen', 'oft' ),
+			'singular_name' => __( 'Allergeen', 'oft' ),
+			'all_items' => __( 'Alle allergenen', 'oft' ),
+			'parent_item' => __( 'Allergeen', 'oft' ),
+			'parent_item_colon' => __( 'Allergeen:', 'oft' ),
+			'new_item_name' => __( 'Nieuw allergeen', 'oft' ),
+			'add_new_item' => __( 'Voeg nieuw allergeen toe', 'oft' ),
+		);
+
+		$args = array(
+			'labels' => $labels,
+			'description' => 'Markeer dat het product dit allergeen bevat',
+			'public' => true,
+			'publicly_queryable' => true,
+			'hierarchical' => true,
+			'show_ui' => true,
+			'show_in_menu' => true,
+			'show_in_nav_menus' => true,
+			'show_in_rest' => true,
+			'show_tagcloud' => true,
+			'show_in_quick_edit' => true,
+			'show_admin_column' => true,
+			'query_var' => true,
+			// Allergenen zullen in principe nooit meer toegevoegd moeten worden, dus catmans enkel rechten geven op toekenning
+			'capabilities' => array( 'assign_terms' => 'edit_products', 'edit_terms' => 'create_sites', 'manage_terms' => 'create_sites', 'delete_terms' => 'create_sites' ),
+			'rewrite' => array( 'slug' => 'allergen', 'with_front' => false ),
+		);
+
+		register_taxonomy( $taxonomy_name, 'product', $args );
+		register_taxonomy_for_object_type( $taxonomy_name, 'product' );
+	}
+
+	// Maak onze custom taxonomiën beschikbaar in menu editor
+	add_filter('woocommerce_attribute_show_in_nav_menus', 'register_custom_taxonomies_for_menus', 1, 2 );
+
+	function register_custom_taxonomies_for_menus( $register, $name = '' ) {
+		$register = true;
+		return $register;
+	}
+
+	// Verhinder bepaalde selecties in de back-end AAN TE PASSEN NAAR DE NIEUWE ID'S
+	add_action( 'admin_footer', 'disable_custom_checkboxes' );
+
+	function disable_custom_checkboxes() {
+		?>
+		<script>
+			/* Disable hoofdcategorieën */
+			jQuery( '#in-product_cat-200' ).prop( 'disabled', true );
+			jQuery( '#in-product_cat-204' ).prop( 'disabled', true );
+			jQuery( '#in-product_cat-210' ).prop( 'disabled', true );
+			jQuery( '#in-product_cat-213' ).prop( 'disabled', true );
+			jQuery( '#in-product_cat-224' ).prop( 'disabled', true );
+			
+			/* Disable continenten */
+			jQuery( '#in-product_partner-162' ).prop( 'disabled', true );
+			jQuery( '#in-product_partner-163' ).prop( 'disabled', true );
+			jQuery( '#in-product_partner-164' ).prop( 'disabled', true );
+			jQuery( '#in-product_partner-165' ).prop( 'disabled', true );
+			
+			/* Disable bovenliggende landen/continenten van alle aangevinkte partners/landen */
+			jQuery( '#taxonomy-product_partner' ).find( 'input[type=checkbox]:checked' ).closest( 'ul.children' ).siblings( 'label.selectit' ).find( 'input[type=checkbox]' ).prop( 'disabled', true );
+
+			/* Disable/enable het bovenliggende land bij aan/afvinken van een partner */
+			jQuery( '#taxonomy-product_partner' ).find( 'input[type=checkbox]' ).on( 'change', function() {
+				jQuery(this).closest( 'ul.children' ).siblings( 'label.selectit' ).find( 'input[type=checkbox]' ).prop( 'disabled', jQuery(this).is(":checked") );
+			});
+
+			/* Disable allergeenklasses */
+			jQuery( '#in-product_allergen-170' ).prop( 'disabled', true );
+			jQuery( '#in-product_allergen-171' ).prop( 'disabled', true );
+
+			/* Disable rode en witte druiven */
+			jQuery( '#in-product_grape-1724' ).prop( 'disabled', true );
+			jQuery( '#in-product_grape-1725' ).prop( 'disabled', true );
+		</script>
+		<?php
+	}
+
+	// Toon metaboxes voor wijninfo enkel voor producten onder de hoofdcategorie 'Wijn'
+	add_action( 'admin_init', 'hide_wine_taxonomies' );
+
+	function hide_wine_taxonomies() {
+		if ( isset($_GET['action']) and $_GET['action'] === 'edit' ) {
+			$post_id = isset( $_GET['post'] ) ? $_GET['post'] : $_POST['post_ID'];
+			$categories = get_the_terms( $post_id, 'product_cat' );
+			if ( is_array( $categories ) ) {
+				foreach ( $categories as $category ) {
+					while ( $category->parent !== 0 ) {
+						$parent = get_term( $category->parent, 'product_cat' );
+						$category = $parent;
+					}
+				}
+				if ( $parent->slug !== 'wijn' ) {
+					remove_meta_box('product_grapediv', 'product', 'normal');
+					remove_meta_box('product_recipediv', 'product', 'normal');
+					remove_meta_box('product_tastediv', 'product', 'normal');
+				}
+			}
+		}
+	}
+
+
+	###########
+	#  VARIA  #
+	###########
+
 	function create_product_pdf( $product ) {
 		require_once WP_CONTENT_DIR.'/plugins/html2pdf/html2pdf.class.php';
 		
@@ -142,6 +342,11 @@
 		$pdffile->WriteHTML($templatecontent);
 		$pdffile->Output( WP_CONTENT_DIR."/".$sku.".pdf", "F" );
 	}
+
+
+	###############
+	#  DEBUGGING  #
+	###############
 
 	// Print variabelen op een overzichtelijke manier naar debug.log
 	if ( ! function_exists( 'write_log' ) ) {
