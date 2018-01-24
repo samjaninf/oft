0c00ef1fcc1ecfe4d9b4ec0ff423477d36e6ea07
Macbook Pro 13 <freeaanzee@gmail.com>
2018-01-24 18:02:33 +0100

    Breadcrumbs gelijktrekken

--- plugins/unyson/framework/extensions/breadcrumbs/includes/class-breadcrumbs-builder.php
+++ plugins/unyson/framework/extensions/breadcrumbs/includes/class-breadcrumbs-builder.php
@@ -95,7 +95,9 @@ class Breadcrumbs_Builder {
 
 		$return = array(
 			0 => array(
-				'name' => $this->settings['labels']['homepage-title'],
+				// GEWIJZIGD: Titel rechtstreeks overschijrven (werkt niet via settings)
+				// 'name' => $this->settings['labels']['homepage-title'],
+				'name' => 'Home',
 				'url'  => esc_url( home_url('/') ),
 				'type' => 'front_page'
 			),
