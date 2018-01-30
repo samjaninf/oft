a1d5e4fe6b25ae755ab0c386c26fc7967888a49e
Macbook Pro 15 <freeaanzee@gmail.com>
2018-01-30 17:30:30 +0100

    Enkel redirecten indien zinvolle target

--- functions.php
+++ functions.php
@@ -103,12 +103,18 @@
 		<script>
 			jQuery(document).ready( function() {
 				jQuery( '.oft-link-target' ).click( function() {
-					window.location.href = jQuery(this).find( '.vc_btn3-shape-rounded.vc_btn3-style-flat' ).attr('href'); 
-					return false;
+					var href = jQuery(this).find( '.vc_btn3-shape-rounded.vc_btn3-style-flat' ).attr('href');
+					if ( href.count > 5 ) {
+						window.location.href = href;
+						return false;
+					}
 				});
 				jQuery( '.oft-link-target-title' ).click( function() {
-					window.location.href = jQuery(this).find( '.wpb_text_column > .wpb_wrapper > h2 > a' ).attr('href'); 
-					return false;
+					var href = jQuery(this).find( '.wpb_text_column > .wpb_wrapper > h2 > a' ).attr('href');
+					if ( href.count > 5 ) {
+						window.location.href = href;
+						return false;
+					}
 				});
 				// jQuery( '.featured-box-title' ).addClass( 'oft-title-size' );
 			});
