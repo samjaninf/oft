5b3a812b14e54f8b05604e1a6ca9d873f6025764
Mac Mini <freeaanzee@gmail.com>
2018-02-04 12:47:51 +0100

    Naar 1.2.2

--- header-404.php
+++ header-404.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html style="min-height: 100%">
+<html style="height: 100%">
 <head>
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
@@ -7,8 +7,8 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
 	<?php wp_head(); ?>
 </head>
-<body <?php body_class(); ?> itemscope="itemscope" itemtype="http://schema.org/WebPage" style="min-height: 100%;">
-<div id="page" class="site" style="min-height: 100%;">
+<body <?php body_class(); ?> itemscope="itemscope" itemtype="http://schema.org/WebPage" style="height: 100%;">
+<div id="page" class="site" style="height: 100%;">
 	<?php alone_header_mobile_menu(); ?>
 	<?php alone_header(); ?>
-	<div id="main" class="site-main" style="min-height: 100%;">
+	<div id="main" class="site-main" style="height: 100%;">
