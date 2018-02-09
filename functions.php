af99892b750f9896da629eea293c78b165be5ac3
Mac Mini <freeaanzee@gmail.com>
2018-02-09 10:29:29 +0100

    Fichecreatie tijdelijk uitschakelen

--- functions.php
+++ functions.php
@@ -1260,8 +1260,8 @@
 		if ( get_option('oft_import_active') !== 'yes' ) {
 			// Enkel proberen aanmaken indien OFT-product én foto reeds aanwezig
 			if ( $product->get_attribute('merk') === 'Oxfam Fair Trade' and intval( $product->get_image_id() ) > 0 ) {
-				create_product_pdf( $product->get_id(), 'nl' );
-				create_product_pdf( $product->get_id(), 'fr' );
+				// create_product_pdf( $product->get_id(), 'nl' );
+				// create_product_pdf( $product->get_id(), 'fr' );
 				// create_product_pdf( $product->get_id(), 'en' );
 			}
 		}
