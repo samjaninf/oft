7f5191ca28c7c87ed76c3993fc602aa31ca9b39a
Macbook Pro 13 <freeaanzee@gmail.com>
2018-02-12 19:35:03 +0100

    Tweaks

--- content-none.php
+++ content-none.php
@@ -6,17 +6,17 @@
 <div class="entry-content bt-content-none" itemprop="text">
 	<h2 class="entry-title"><?php esc_html_e( 'Niets gevonden!', 'oft' ); ?></h2>
 	<?php if ( is_search() ) : ?>
-		<p><?php esc_html_e( 'We konden helaas niets vinden met die zoektermen. Probeer het eens opnieuw met een korter woord, of volg de automatische suggestie hierboven.', 'oft' ); ?></p>
+		<p><?php echo __( 'We konden helaas niets vinden met die zoektermen.', 'oft' ).'<br>'.__( 'Probeer het eens opnieuw met een korter woord, of volg de automatische suggestie hierboven.', 'oft' ); ?></p>
 		<div class="row">
-			<div class="col-md-6">
+			<div class="col-md-12" style="text-align: center;">
 				<?php get_search_form(); ?>
 			</div>
 		</div>
 		<div class="clearfix"></div>
 	<?php else : ?>
-		<p><?php esc_html_e( 'Die pagina kunnen we niet vinden. Misschien kan een kleine zoektocht raad brengen?', 'oft' ); ?></p>
+		<p><?php echo __( 'Die pagina kunnen we niet vinden.', 'oft' ).'<br>'.__( 'Misschien kan een kleine zoektocht raad brengen?', 'oft' ); ?></p>
 		<div class="row">
-			<div class="col-md-6">
+			<div class="col-md-12" style="text-align: center;">
 				<?php get_search_form(); ?>
 			</div>
 		</div>
