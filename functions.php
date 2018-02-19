1b111c6dee020978b4906123e5cce2423930be8e
Macbook Pro 13 <freeaanzee@gmail.com>
2018-02-19 19:29:11 +0100

    Externe logo's klein genoeg houden

--- functions.php
+++ functions.php
@@ -55,26 +55,6 @@
 		// Fix error 60 - SSL certificate problem: unable to get local issuer certificate (bij het downloaden van een CSV in WP All Import)
 		curl_setopt( $handle, CURLOPT_SSL_VERIFYPEER, false );
 	}
-	
-	// Allerhande tweaks na het initialiseren van WordPress
-	// add_action( 'init', 'remove_storefront_actions', 20 );
-	
-	function remove_storefront_actions() {
-		// Verwijder de productzoeker en de mini-cart in de header rechtsboven
-		remove_action( 'storefront_header', 'storefront_product_search', 40 );
-		remove_action( 'storefront_header', 'storefront_header_cart', 60 );
-
-		// Laat de categorie- en auteurinfo vallen bij blogposts
-		remove_action( 'storefront_loop_post', 'storefront_post_meta', 20 );
-		remove_action( 'storefront_single_post', 'storefront_post_meta', 20 );
-
-		// Verwijder alle buttons om te kopen
-		remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart' );
-		remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
-		
-		// Verwijder de default credits in de footer
-		remove_action( 'storefront_footer', 'storefront_credit', 20 );
-	}
 
 	// Allerhande tweaks na het initialiseren van WordPress
 	add_action( 'init', 'remove_alone_actions', 20 );
@@ -1457,7 +1437,7 @@
 				array( 
 					'id' => '_update_product_sheet',
 					'label' => __( 'Fiches updaten', 'oft' ),
-					'description' => __( 'Vink dit aan als je de PDF\'s wil bijwerken tijdens het opslaan (ook niet-OFT-producten)', 'oft' ),
+					'description' => __( 'Vink dit aan als je de PDF\'s wil bijwerken wanneer je het product opslaat (werkt ook voor niet-OFT-producten!)', 'oft' ),
 				)
 			);
 
