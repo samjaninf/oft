4cb1faeee15992a3b50ca11ca9f4dec0951c922a
Macbook Pro 13 <freeaanzee@gmail.com>
2018-03-05 13:35:06 +0100

    Update WC-templates naar 3.3.0

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -2,16 +2,14 @@
 /**
  * The Template for displaying product archives, including the main shop page which is a post type archive
  *
- * This template can be overridden by copying it to yourtheme/woocommerce/archive-product.php.
- *
  * @see 	    https://docs.woocommerce.com/document/template-structure/
  * @author 		WooThemes
  * @package 	WooCommerce/Templates
- * @version     2.0.0
+ * @version     3.3.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
+	exit;
 }
 
 $alone_customizer_shop_option = function_exists('fw_get_db_customizer_option') ? fw_get_db_customizer_option('shop_settings') : array('products_in_row' => 4);
@@ -29,36 +27,38 @@ $alone_sidebar_position = function_exists( 'fw_ext_sidebars_get_current_position
 					<?php if ( function_exists('woocommerce_breadcrumb') ) woocommerce_breadcrumb(); ?>
 					<?php
 						/**
-						 * woocommerce_before_main_content hook.
+						 * Hook: woocommerce_before_main_content.
 						 *
 						 * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
 						 * @hooked woocommerce_breadcrumb - 20
+						 * @hooked WC_Structured_Data::generate_website_data() - 30
 						 */
 						do_action( 'woocommerce_before_main_content' );
 					?>
-
+					
+					<header class="woocommerce-products-header">
 						<?php if ( apply_filters( 'woocommerce_show_page_title', false ) ) : ?>
-
-							<h1 class="page-title"><?php woocommerce_page_title(); ?></h1>
-
+							<h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
 						<?php endif; ?>
 
 						<?php
 							/**
-							 * woocommerce_archive_description hook.
+							 * Hook: woocommerce_archive_description.
 							 *
 							 * @hooked woocommerce_taxonomy_archive_description - 10
 							 * @hooked woocommerce_product_archive_description - 10
 							 */
 							do_action( 'woocommerce_archive_description' );
 						?>
-
+					</header>
+					
 						<?php if ( have_posts() ) : ?>
 
 							<?php
 								/**
-								 * woocommerce_before_shop_loop hook.
+								 * Hook: woocommerce_before_shop_loop.
 								 *
+								 * @hooked wc_print_notices - 10
 								 * @hooked woocommerce_result_count - 20
 								 * @hooked woocommerce_catalog_ordering - 30
 								 */
@@ -66,35 +66,56 @@ $alone_sidebar_position = function_exists( 'fw_ext_sidebars_get_current_position
 							?>
 
 							<?php woocommerce_product_loop_start(); ?>
-								<div class="woocommerce-product-subcategories-wrap">
+							
+							<div class="woocommerce-product-subcategories-wrap">
 								<?php woocommerce_product_subcategories(); ?>
-								</div>
+							</div>
+
+							<?php if ( wc_get_loop_prop( 'total' ) ) : ?>
 								<div class="bt-row">
-									<?php while ( have_posts() ) : the_post();
-									?><div class="bt-col-<?php echo esc_attr( (int) $alone_customizer_shop_option['products_in_row']); ?> product-item">
-										  <?php wc_get_template_part( 'content', 'product' ); ?>
-						      </div><?php endwhile; // end of the loop. ?>
+									<?php while ( have_posts() ) : the_post(); ?>
+										<?php
+											/**
+											 * Hook: woocommerce_shop_loop.
+											 *
+											 * @hooked WC_Structured_Data::generate_product_data() - 10
+											 */
+											do_action( 'woocommerce_shop_loop' );
+										?>
+										<div class="bt-col-<?php echo esc_attr( (int) $alone_customizer_shop_option['products_in_row']); ?> product-item">
+											<?php wc_get_template_part( 'content', 'product' ); ?>
+										</div>
+									<?php endwhile; ?>
 								</div>
+							<?php endif; ?>
+							
 							<?php woocommerce_product_loop_end(); ?>
 
 							<?php
 								/**
-								 * woocommerce_after_shop_loop hook.
+								 * Hook: woocommerce_after_shop_loop.
 								 *
 								 * @hooked woocommerce_pagination - 10
 								 */
 								do_action( 'woocommerce_after_shop_loop' );
 							?>
 
-						<?php elseif ( ! woocommerce_product_subcategories( array( 'before' => woocommerce_product_loop_start( false ), 'after' => woocommerce_product_loop_end( false ) ) ) ) : ?>
+						<?php else : ?>
 
-							<?php wc_get_template( 'loop/no-products-found.php' ); ?>
+							<?php
+								/**
+								 * Hook: woocommerce_no_products_found.
+								 *
+								 * @hooked wc_no_products_found - 10
+								 */
+								do_action( 'woocommerce_no_products_found' );
+							?>
 
 						<?php endif; ?>
 
 					<?php
 						/**
-						 * woocommerce_after_main_content hook.
+						 * Hook: woocommerce_after_main_content.
 						 *
 						 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
 						 */
@@ -103,7 +124,7 @@ $alone_sidebar_position = function_exists( 'fw_ext_sidebars_get_current_position
 
 					<?php
 						/**
-						 * woocommerce_sidebar hook.
+						 * Hook: woocommerce_sidebar.
 						 *
 						 * @hooked woocommerce_get_sidebar - 10
 						 */
