79afe0683c88179c24a824f872723450ecc0141a
Macbook Pro 13 <info@fullstackahead.be>
2018-09-10 18:42:53 +0200

    Catmans moeten wel producten kunnen aanmaken!

--- functions.php
+++ functions.php
@@ -57,7 +57,7 @@
 	add_filter( 'woocommerce_register_post_type_shop_order', 'disable_post_creation' );
 	
 	function disable_post_creation( $fields ) {
-		if ( ! current_user_can('update_core') ) {
+		if ( ! current_user_can('manage_woocommerce') ) {
 			$fields['capabilities'] = array(
 				'create_posts' => false,
 			);
