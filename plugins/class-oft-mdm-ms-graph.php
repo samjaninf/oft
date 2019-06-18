dc65dade9d5082c1e745fb906748dcb4158633e3
Macbook Pro 15 <info@fullstackahead.be>
2019-06-19 00:02:20 +0200

    Gebruik klasseconstanten

--- plugins/class-oft-mdm-ms-graph.php
+++ plugins/class-oft-mdm-ms-graph.php
@@ -1,4 +1,6 @@
 <?php
+	use Microsoft\Graph;
+
 	$start = microtime(true);
 
 	require_once '../../../../wp-load.php';
@@ -7,21 +9,20 @@
 	new Oft_Mdm_Microsoft_Graph();
 
 	class Oft_Mdm_Microsoft_Graph {
-		static $graph, $user_name, $user_id, $oww_scheme_id, $mdm_scheme_id, $shuttle_scheme_id, $context;
+		protected $graph, $context;
+		
+		const USER_NAME = 'klantendienst@oft.be';
+		const USER_ID = '3a4ec597-1540-4a6a-81d9-2d9ea893edb0';
+		const oww_scheme_id = 'AAMkADNlZGNlY2Q3LTU4NDctNDZlMi1hMjgyLWNjMTdhN2NiZTk0ZgBGAAAAAABuULMO0-qUSYSIyb2KIl9LBwAoJ3qpsWFSSpypVo542E_lAAAAAAEGAAAoJ3qpsWFSSpypVo542E_lAAAcuXwlAAA=';
+		const mdm_scheme_id = 'AAMkADNlZGNlY2Q3LTU4NDctNDZlMi1hMjgyLWNjMTdhN2NiZTk0ZgBGAAAAAABuULMO0-qUSYSIyb2KIl9LBwAoJ3qpsWFSSpypVo542E_lAAAAAAEGAAAoJ3qpsWFSSpypVo542E_lAAAcuXwmAAA=';
+		const shuttle_scheme_id = 'AAMkADNlZGNlY2Q3LTU4NDctNDZlMi1hMjgyLWNjMTdhN2NiZTk0ZgBGAAAAAABuULMO0-qUSYSIyb2KIl9LBwAoJ3qpsWFSSpypVo542E_lAAAAAAEGAAAoJ3qpsWFSSpypVo542E_lAAAcuXwnAAA=';
 
 		function __construct() {
-			self::$user_name = 'klantendienst@oft.be';
-			self::$user_id = '3a4ec597-1540-4a6a-81d9-2d9ea893edb0';
-			self::$oww_scheme_id = 'AAMkADNlZGNlY2Q3LTU4NDctNDZlMi1hMjgyLWNjMTdhN2NiZTk0ZgBGAAAAAABuULMO0-qUSYSIyb2KIl9LBwAoJ3qpsWFSSpypVo542E_lAAAAAAEGAAAoJ3qpsWFSSpypVo542E_lAAAcuXwlAAA=';
-			self::$mdm_scheme_id = 'AAMkADNlZGNlY2Q3LTU4NDctNDZlMi1hMjgyLWNjMTdhN2NiZTk0ZgBGAAAAAABuULMO0-qUSYSIyb2KIl9LBwAoJ3qpsWFSSpypVo542E_lAAAAAAEGAAAoJ3qpsWFSSpypVo542E_lAAAcuXwmAAA=';
-			self::$shuttle_scheme_id = 'AAMkADNlZGNlY2Q3LTU4NDctNDZlMi1hMjgyLWNjMTdhN2NiZTk0ZgBGAAAAAABuULMO0-qUSYSIyb2KIl9LBwAoJ3qpsWFSSpypVo542E_lAAAAAAEGAAAoJ3qpsWFSSpypVo542E_lAAAcuXwnAAA=';
-			self::$context = array( 'source' => 'Microsoft Graph API' );
-
 			global $start;
 			require_once WP_CONTENT_DIR.'/plugins/microsoft-graph/autoload.php';
 			echo number_format( microtime(true)-$start, 4, ',', '.' )." s => MS GRAPH API LOADED<br/>";
 			
-			$guzzle = new GuzzleHttp\Client();
+			$guzzle = new \GuzzleHttp\Client();
 			$token = json_decode( $guzzle->post(
 				'https://login.microsoftonline.com/'.MS_TENANT_ID.'/oauth2/token?api-version=1.0',
 				array(
@@ -35,13 +36,14 @@
 			)->getBody()->getContents() );
 			echo number_format( microtime(true)-$start, 4, ',', '.' )." s => ACCESS TOKEN RECEIVED<br/>";
 
-			self::$graph = new Microsoft\Graph\Graph();
-			self::$graph->setBaseUrl('https://graph.microsoft.com')->setApiVersion('v1.0')->setAccessToken( $token->access_token );
+			$this->graph = new Graph();
+			$this->graph->setBaseUrl('https://graph.microsoft.com')->setApiVersion('v1.0')->setAccessToken( $token->access_token );
+			$this->context = array( 'source' => 'Microsoft Graph API' );
 
 			// Kalender-ID's veranderen per gebruiker!
 			// $user_name_app = 'oxfamappuser@oww.be';
 			// $user_id_app = 'b4de7e02-104b-42b4-967d-484b7fcde90d';
-			// $calendars = self::$graph
+			// $calendars = $this->graph
 			// 	->createRequest( 'GET', '/users/'.$user_name_app.'/calendars' )
 			// 	->addHeaders( array( 'Content-Type' => 'application/json', 'Prefer' => 'outlook.timezone="Europe/Paris"' ) )
 			// 	->setReturnType(Microsoft\Graph\Model\Event::class)
@@ -51,14 +53,20 @@
 
 			try {
 				// Let op het verschil tussen events en de instances van terugkerende events!
-				$event_a1a = $this->get_calendar_event_by_routecode();
+
+				if ( isset( $_GET['routecode'] ) and ! empty( $_GET['routecode'] ) ) {
+					$routecode = $_GET['routecode'];
+				} else {
+					$routecode = NULL;
+				}
+				$event_a1a = $this->get_calendar_event_by_routecode( $routecode );
 				echo number_format( microtime(true)-$start, 4, ',', '.' )." s => EVENT REQUEST EXECUTED<br/>";
 
 				$instances = $this->get_instances_for_calendar_event( $event_a1a->getId() );
 				echo number_format( microtime(true)-$start, 4, ',', '.' )." s => INSTANCE REQUEST EXECUTED<br/>";
 				echo "<br/>".count($instances)." INSTANCES<br/>";
 
-				$instances = $this->get_calendar_view_by_subject( NULL, 50 );
+				$instances = $this->get_calendar_view_by_subject( $routecode, 50 );
 				echo "<br/>".count($instances)." INSTANCES<br/>";
 
 				foreach ( $instances as $event ) {
@@ -76,9 +84,9 @@
 			$logger = wc_get_logger();
 
 			// Single quotes gebruiken zodat dollartekens niet als variabelen geïnterpreteerd worden
-			$events = self::$graph->createRequest( 'GET', '/users/'.self::$user_name.'/calendars/'.self::$oww_scheme_id.'/events?$orderby=start/dateTime asc&$filter=categories/any(a:a eq \'Z'.$routecode.'\') and startswith(subject,\''.$type.'\')' )
+			$events = $this->graph->createRequest( 'GET', '/users/'.self::USER_NAME.'/calendars/'.self::OWW_SCHEME_ID.'/events?$orderby=start/dateTime asc&$filter=categories/any(a:a eq \'Z'.$routecode.'\') and startswith(subject,\''.$type.'\')' )
 				->addHeaders( array( 'Content-Type' => 'application/json', 'Prefer' => 'outlook.timezone="Europe/Paris"' ) )
-				->setReturnType(Microsoft\Graph\Model\Event::class)
+				->setReturnType(Model\Event::class)
 				->setTimeout(10000)
 				->execute();
 
@@ -87,11 +95,11 @@
 					return $events[0];
 				} elseif ( count( $events ) > 1 ) {
 					$msg = 'MULTIPLE EVENTS FOUND';
-					$logger->$warning( $msg, self::$context );
+					$logger->$warning( $msg, $this->context );
 					return $msg;
 				}
 			} else {
-				$logger->$warning( 'Fatal error', self::$context );
+				$logger->$warning( 'Fatal error', $this->context );
 			}
 
 			return false;
@@ -105,9 +113,9 @@
 			var_dump_pre($start_date);
 			var_dump_pre($end_date);
 
-			$instances = self::$graph->createRequest( 'GET', '/users/'.self::$user_name.'/calendars/'.self::$oww_scheme_id.'/events/'.$event_id.'/instances?startDateTime='.$start_date.'&endDateTime='.$end_date.'&$orderby=start/dateTime asc&$top='.$limit )
+			$instances = $this->graph->createRequest( 'GET', '/users/'.self::USER_NAME.'/calendars/'.self::OWW_SCHEME_ID.'/events/'.$event_id.'/instances?startDateTime='.$start_date.'&endDateTime='.$end_date.'&$orderby=start/dateTime asc&$top='.$limit )
 				->addHeaders( array( 'Content-Type' => 'application/json', 'Prefer' => 'outlook.timezone="Europe/Paris"' ) )
-				->setReturnType(Microsoft\Graph\Model\Event::class)
+				->setReturnType(Model\Event::class)
 				->setTimeout(10000)
 				->execute();
 			
@@ -116,11 +124,11 @@
 					return $instances;
 				} else {
 					$msg = 'NO INSTANCES FOUND';
-					$logger->$warning( $msg, self::$context );
+					$logger->$warning( $msg, $this->context );
 					return $msg;
 				}
 			} else {
-				$logger->$warning( 'Fatal error', self::$context );
+				$logger->$warning( 'Fatal error', $this->context );
 			}
 
 			return false;
@@ -136,9 +144,9 @@
 
 			// Opgelet: startswith() is hier case sensitive!
 			// Filteren op categorie resulteert in Error 500
-			$instances_in_view = self::$graph->createRequest( 'GET', '/users/'.self::$user_name.'/calendars/'.self::$oww_scheme_id.'/calendarView?startDateTime='.$start_date.'&endDateTime='.$end_date.'&$orderby=start/dateTime asc&$filter=startswith(subject,\''.$type.'\')&$top='.$limit )
+			$instances_in_view = $this->graph->createRequest( 'GET', '/users/'.self::USER_NAME.'/calendars/'.self::OWW_SCHEME_ID.'/calendarView?startDateTime='.$start_date.'&endDateTime='.$end_date.'&$orderby=start/dateTime asc&$filter=startswith(subject,\''.$type.'\')&$top='.$limit )
 				->addHeaders( array( 'Content-Type' => 'application/json', 'Prefer' => 'outlook.timezone="Europe/Paris"' ) )
-				->setReturnType(Microsoft\Graph\Model\Event::class)
+				->setReturnType(Model\Event::class)
 				->setTimeout(100000)
 				->execute();
 			
@@ -147,11 +155,11 @@
 					return $instances_in_view;
 				} else {
 					$msg = 'NO INSTANCES IN VIEW';
-					$logger->$warning( $msg, self::$context );
+					$logger->$warning( $msg, $this->context );
 					return $msg;
 				}
 			} else {
-				$logger->$warning( 'Fatal error', self::$context );
+				$logger->$warning( 'Fatal error', $this->context );
 			}
 
 			return false;
