4d62d8d7ba30bbe4f36859491b89283cf0358663
Mac Pro <info@fullstackahead.be>
2019-07-18 13:32:35 +0200

    Verwijder checkbox om verzendadres optioneel te maken

---
+++ woocommerce/checkout/form-shipping.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Checkout shipping information form
+ *
+ * @see     https://docs.woocommerce.com/document/template-structure/
+ * @author  WooThemes
+ * @package WooCommerce/Templates
+ * @version 3.0.9
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+?>
+<div class="woocommerce-shipping-fields">
+	<?php if ( true === WC()->cart->needs_shipping_address() ) : ?>
+
+		<h3 id="ship-to-different-address">
+			<!-- GEWIJZIGD: Checkbox altijd aangevinkt, onzichtbaar en onbewerkbaar -->
+			<label class="woocommerce-form__label woocommerce-form__label-for-checkbox checkbox" style="display: none;">
+				<input id="ship-to-different-address-checkbox" class="woocommerce-form__input woocommerce-form__input-checkbox input-checkbox" checked="checked" disabled="disabled" type="checkbox" name="ship_to_different_address" value="1" />
+			</label>
+			<!-- GEWIJZIGD: Span weg en titel uit label trekken -->
+			<?php _e( 'Ship to a different address?', 'woocommerce' ); ?>
+		</h3>
+
+		<div class="shipping_address">
+
+			<?php do_action( 'woocommerce_before_checkout_shipping_form', $checkout ); ?>
+
+			<div class="woocommerce-shipping-fields__field-wrapper">
+				<?php
+					$fields = $checkout->get_checkout_fields( 'shipping' );
+
+					foreach ( $fields as $key => $field ) {
+						if ( isset( $field['country_field'], $fields[ $field['country_field'] ] ) ) {
+							$field['country'] = $checkout->get_value( $field['country_field'] );
+						}
+						woocommerce_form_field( $key, $field, $checkout->get_value( $key ) );
+					}
+				?>
+			</div>
+
+			<?php do_action( 'woocommerce_after_checkout_shipping_form', $checkout ); ?>
+
+		</div>
+
+	<?php endif; ?>
+</div>
+<div class="woocommerce-additional-fields">
+	<?php do_action( 'woocommerce_before_order_notes', $checkout ); ?>
+
+	<?php if ( apply_filters( 'woocommerce_enable_order_notes_field', 'yes' === get_option( 'woocommerce_enable_order_comments', 'yes' ) ) ) : ?>
+
+		<?php if ( ! WC()->cart->needs_shipping() || wc_ship_to_billing_address_only() ) : ?>
+
+			<h3><?php _e( 'Additional information', 'woocommerce' ); ?></h3>
+
+		<?php endif; ?>
+
+		<div class="woocommerce-additional-fields__field-wrapper">
+			<?php foreach ( $checkout->get_checkout_fields( 'order' ) as $key => $field ) : ?>
+				<?php woocommerce_form_field( $key, $field, $checkout->get_value( $key ) ); ?>
+			<?php endforeach; ?>
+		</div>
+
+	<?php endif; ?>
+
+	<?php do_action( 'woocommerce_after_order_notes', $checkout ); ?>
+</div>
